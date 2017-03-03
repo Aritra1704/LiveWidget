3ac5bc62b9ad99c46025f8d93c4e3a9a80e4f626
Aritra1704 <aritra1704@gmail.com>
2017-03-04 02:27:51 +0530

    Implemented Multidex.

--- app/src/main/java/com/arpaul/livewidget/common/ApplicationInstance.java
+++ app/src/main/java/com/arpaul/livewidget/common/ApplicationInstance.java
@@ -1,11 +1,14 @@
 package com.arpaul.livewidget.common;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 /**
  * Created by Aritra on 27-07-2016.
  */
-public class ApplicationInstance extends Application {
+public class ApplicationInstance  extends MultiDexApplication {
 
     public static final int LOADER_FETCH_JSON_DATA      = 1;
     public static final int LOADER_DELETE               = 2;
@@ -16,4 +19,10 @@ public class ApplicationInstance extends Application {
 
     public static final String LOCK_APP_DB              = "LOCK_APP_DB";
     public static final String LOCK_CLICK               = "LOCK_CLICK";
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 }
